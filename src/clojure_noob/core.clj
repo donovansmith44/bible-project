6e08acf2e9894dd5b4aaf543876965fd19ee4988
donovansmith44 <donovan.smith44@gmail.com>
2026-01-05 13:52:29 -0500

    Updating

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,38 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn get-references-associated-with-bible-books []
+  (map-books-to-references {} (get-bible-xrefd-book-names)))
+
+(defn get-bible-xrefd-book-names []
+  (let [matches (re-seq bible-xrefd-book-pattern bible-xrefs)]
+    (->> matches
+         (map second)
+         (map str))))
+
+(def bible-xrefs (slurp "resources/all-cross-references.txt"))
+
+(def bible-xrefd-book-pattern #"\| ((?s).*?) Cross References \|")
+
+
+(defn map-books-to-references [map books]
+  (if (empty? books)
+    map
+    (map-books-to-references
+     (add-book-to-map (first books) map)
+     (rest books))))
+
+(defn add-book-to-map [book map]
+  (assoc map book (get-bible-xrefs book)))
+
+(defn get-bible-xrefs [book-name]
+  (re-seq
+   (re-pattern
+    (str
+     "("
+     book-name
+     " \\d+(?s).*?)(?="
+     book-name
+     " \\d+|$)"))
+  bible-xrefs))
