01098b1e1f723b056b302fcbcd2ab607ff5bdaf9
donovansmith44 <donovan.smith44@gmail.com>
2026-01-06 21:32:37 -0500

    Writing initial tests

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -6,29 +6,7 @@
   [& args]
   (println "Hello, World!"))
 
-(defn get-references-associated-with-bible-books []
-  (map-books-to-references {} (get-bible-xrefd-book-names)))
-
-(defn get-bible-xrefd-book-names []
-  (let [matches (re-seq bible-xrefd-book-pattern bible-xrefs)]
-    (->> matches
-         (map second)
-         (map str))))
-
 (def bible-xrefs (slurp "resources/all-cross-references.txt"))
-
-(def bible-xrefd-book-pattern #"\| ((?s).*?) Cross References \|")
-
-
-(defn map-books-to-references [map books]
-  (if (empty? books)
-    map
-    (map-books-to-references
-     (add-book-to-map (first books) map)
-     (rest books))))
-
-(defn add-book-to-map [book map]
-  (assoc map book (get-bible-xrefs book)))
 
 (defn get-bible-xrefs [book-name]
   (re-seq
@@ -40,3 +18,25 @@
      book-name
      " \\d+|$)"))
   bible-xrefs))
+
+(defn add-book-to-map [book map]
+  (assoc map book (get-bible-xrefs book)))
+
+
+(defn map-books-to-references [map books]
+  (if (empty? books)
+    map
+    (map-books-to-references
+     (add-book-to-map (first books) map)
+     (rest books))))
+
+(def bible-xrefd-book-pattern #"\| ((?s).*?) Cross References \|")
+
+(defn get-bible-xrefd-book-names []
+  (let [matches (re-seq bible-xrefd-book-pattern bible-xrefs)]
+    (->> matches
+         (map second)
+         (map str))))
+
+(defn get-references-associated-with-bible-books []
+  (map-books-to-references {} (get-bible-xrefd-book-names)))
--- test/clojure_noob/core_test.clj
+++ test/clojure_noob/core_test.clj
@@ -2,6 +2,67 @@
   (:require [clojure.test :refer :all]
             [clojure-noob.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def xref-map-for-arbitrary-chapter ; Psalm 117
+  { :verses
+   { :1 {:a #{ {
+                :book    "Romans"
+                :chapter 15
+                :verse   11
+                :type    :citation ;; specifically, Romans cites Psalm 117:1
+                }}}
+    :2  {:b #{ {
+                :book    "Psalms"
+                :chapter 93
+                :verse   11
+                :type    :phraseological
+                }
+              {
+               :book    "Psalms"
+               :chapter 116
+               :verse   5
+               :type    :thematic-proximal
+               ;; maybe :type :comparative
+               }}
+         :c #{ {
+                :book    "Psalms"
+                :chapter 100
+                :verse   5
+                :type    :thematic-proximal
+                ;; maybe :type :comparative
+                }}
+         :d #{ {
+                :book    "Exodus"
+                :chapter 34
+                :verse   6
+                :type    :phraseological
+                }}
+         :e #{ {
+                :book    "Psalm"
+                :chapter 116
+                :verse   19
+                :type    :thematic-distal
+                ;; maybe :type :distal-comparative 
+                }}} }  })
+
+(def xref-map-for-arbitrary-verse ; Psalm 117:1
+  (get xref-map-for-arbitrary-chapter :1))
+
+(deftest transform-raw-refs-into-map
+  (testing "Insert raw refs to map call here"
+    ;; SHOULD INCLUDE:
+    ;; Psalm 117 a 117:1 Cited Rm 15:11
+    ;; b 117:2 Ps 103:11; [Ps 116:5]
+    ;; c 117:2 [Ps 100:5]
+    ;; d 117:2 Ex 34:6
+    ;; e 117:2 [See Ps 116:19 above] 
+    (is (= xref-map-for-arbitrary-chapter {}))))
+
+(deftest test-get-refs-for-chapter
+  (testing "Insert get refs for chapter call here"
+  ;; (testing (get-refs-for-chapter Psalm 117)
+    (is (= xref-map-for-arbitrary-chapter {}))))
+
+(deftest test-get-refs-for-verse
+  (testing "Insert get refs for verse call here"
+  ;; (testing (get-refs-for-chapter Psalm 117 1)
+    (is (= xref-map-for-arbitrary-verse {}))))
