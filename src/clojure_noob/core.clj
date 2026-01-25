8d1fd1af4ae17f59f6917c42252bfebf46d84833
donovansmith44 <donovan.smith44@gmail.com>
2026-01-25 12:54:04 -0500

    Writing the instaparse parser for the cross refs

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,12 +1,32 @@
 (ns clojure-noob.core
-  (:gen-class))
+  (:gen-class)
+  (:require [instaparse.core :as insta]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
+(def as-and-bs
+  (insta/parser
+    "S = \\| BOOK_NAME \"\"\\|"))
+
+(def book-names
+  (insta/parser
+   "BOOK_NAME = #\"[A-Z][a-z]+\"
+    CHAPTER_ID = BOOK_NAME #\"\\s+\\d\"
+    BOOK_HEADING = \"| \" BOOK_NAME \" Cross References \" #\"\\|\n\" CHAPTER_ID
+    REF_LETTER = #\"[a-z]\"
+    WHITESPACE = #\"\\s\"
+    XREFD_VERSE = #\"\\d+:\\d+\"
+    XREF = REF_LETTER WHITESPACE+ XREFD_VERSE WHITESPACE
+    BOOK_ABBR = #\"(1|2|3[A-Z])|[A-Z][a-z]+\"
+    CHAPTER = #\"\\d+\"
+    PHRASEOLOGICAL = BOOK_ABBR WHITESPACE CHAPTER "\:" VERSE_NUM WHITESPACE+ REFS
+    REFS = <PHRASEOLOGICAL|CITATION|THEMATIC> "\;""))
+
 (def bible-xrefs (slurp "resources/all-cross-references.txt"))
+(book-names bible-xrefs)
 
 (defn get-bible-xrefs [book-name]
   (re-seq
